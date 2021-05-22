949aa6033d396f3c7c3c6524a92cd772eb9d7f2f
David Tayrone <30438651+mekpower@users.noreply.github.com>
2021-05-22 16:27:06 -0300

    config vuepress nav

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: "M3k Studio Documentations",
+  description: 'blabla',
+  themeConfig: {
+    nav: [
+      {text: 'Home', link: "/"},
+      {text: 'Games', arialLabel: 'Games Menu',
+      items: [
+        {text: 'Better Mine', link: '/bettermine'}
+      ]},
+      {text: 'Home', link: "/"},
+      {text: 'M3K Studio', link: "https://m3kstudio.dtayrone1.repl.co"}
+    ]
+  }
+}
