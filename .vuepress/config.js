27a07b186a89bc93c053fb633b9591a56414e99d
David Tayrone <dtayrone1@gmail.com>
2021-05-23 12:28:35 -0300

    Main page, favcon, style updates
    - Main page to YAML.
    - favicon in '.vuepress/public'.
    - styles in '.vuepress'
    - assets folder created.

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,12 +1,26 @@
 module.exports = {
   title: "M3k Studio Documentations",
   description: 'blabla',
+  head: [
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/favicons/apple-touch-icon.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicons/favicon-32x32.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicons/favicon-16x16.png"}],
+    ['link', { rel: "manifest", href: "/favicons/site.webmanifest"}],
+    ['link', { rel: "mask-icon", href: "/favicons/safari-pinned-tab.svg", color: "#3a0839"}],
+    ['link', { rel: "shortcut icon", href: "/favicons/favicon.ico"}],
+    ['meta', { name: "msapplication-TileColor", content: "#3a0839"}],
+    ['meta', { name: "msapplication-config", content: "/favicons/browserconfig.xml"}],
+    ['meta', { name: "theme-color", content: "#ffffff"}],
+  ],
   themeConfig: {
     nav: [
       {text: 'Home', link: "/"},
       {text: 'Games', arialLabel: 'Games Menu',
       items: [
-        {text: 'Better Mine', link: '/bettermine'}
+        {text: 'Better Mine', arialLabel: 'better mine',
+      items: [
+        {text: '1.0.0', link: "/bettermine/1.0.0"}
+      ]}
       ]},
       {text: 'Home', link: "/"},
       {text: 'M3K Studio', link: "https://m3kstudio.dtayrone1.repl.co"}
